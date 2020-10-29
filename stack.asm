088f466b2a9d11236ed8878e84af905d974233eb
Alexey <alexneudatchin@gmail.com>
2020-10-29 20:16:25 +0300

    not r 4

--- stack.asm
+++ stack.asm
@@ -1,13 +1,15 @@
-.model small
+.model large
 .586
-stackle segment stack 
+stackle segment stack
 there	dw 5
 		dw 7
 		dw 17
-		stacks equ $ - there
+basket  equ $ - there				
+		dw 128-basket dup(0) 
 stackle ends 
 .data
-	put db "boat",'$'
+	put   db "boat", 0ah, 0dh,'$'
+	outpost   db "out of",'$'
 .code
 	main   proc	
 	mov ax, @data
@@ -15,22 +17,25 @@ stackle ends
 	lea ax, put 
 	mov dx, ax
 
+	mov sp, 0
 	pop cx
-
-	multiply:
+multiply:
 	pop ax
 	imul cx, ax
-	cmp sp, stacks
+	cmp sp, basket
 	jl multiply
 
-;	mov cx, 595
-
 	mov bx, dx
-	mov [bx], cl 
-
+	cmp cx, 595
+	jne essa
+	mov al, 'B'
+	mov [bx], al
+essa:
 	mov ah, 09h
 	int 21h
 	mov ax, 4c00h
 	int 21h
+
 	main   endp
 	end main
+
